209af5e87bdad5a0f945de72659f5917891be6f2
Jagpreet Bath <jbath@proserveit.com>
2020-08-05 21:25:01 -0400

    Completed Database Schema and Queries

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -110,3 +110,100 @@ ORDER BY de.dept_no;
 
 Select * from department_count
 
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT emp_no,
+    first_name,
+	last_name,
+    gender
+	INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+	INNER JOIN salaries as s
+		ON (e.emp_no = s.emp_no)
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+SELECT * FROM emp_info
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+SELECT * FROM manager_info
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+SELECT * FROM dept_info
+
+SELECT * FROM departments 
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    departments.dept_name
+FROM retirement_info
+	LEFT JOIN dept_emp 
+		ON retirement_info.emp_no = dept_emp.emp_no
+	LEFT JOIN departments 
+		ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    departments.dept_name
+FROM retirement_info
+	LEFT JOIN dept_emp 
+		ON retirement_info.emp_no = dept_emp.emp_no
+	LEFT JOIN departments 
+		ON dept_emp.dept_no = departments.dept_no
+WHERE (departments.dept_name = 'Sales') 
+OR (departments.dept_name = 'Development');
+
+
+
+
